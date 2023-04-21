8a58081da9787d0bcf201a8d89f5980f6bcc5647
BelialDaNoS <lautarogazzo1@gmail.com>
2023-04-20 21:50:02 -0300

    Casiiiiii D:
    Front-End funcional. Falta agregar el endpoint post para que sume el producto con id X al carrito X. Ejemplo:
    POST http://localhost:8080/api/cart/1/product/1
    
    ------
    Arreglar el endpoint PUT

--- Primer entrega proyecto/src/routes/cart.js
+++ Primer entrega proyecto/src/routes/cart.js
@@ -32,101 +32,89 @@ router.get('/total', (req, res) => {
 
 router.post('/', (req, res) => {
 
-    // Verificar si existe el archivo de carritos y crearlo si no existe
-    if (!fs.existsSync(cartsFilePath)) {
-        fs.writeFileSync(cartsFilePath, JSON.stringify([]));
-    }
+// Verificar si existe el archivo de carritos y crearlo si no existe
+if (!fs.existsSync(cartsFilePath)) {
+  fs.writeFileSync(cartsFilePath, JSON.stringify([]));
+}
 
-    const cartproducts = [];
+const cartproducts = [];
 
-    // Agregar el producto recibido al array de productos
-    const newProduct = req.body;
-    cartproducts.push(newProduct);
+// Agregar el producto recibido al array de productos
+const newProduct = req.body;
 
-    // Leer el archivo de carritos
-    const carts = JSON.parse(fs.readFileSync(cartsFilePath));
+newProduct.precio = parseFloat(newProduct.precio);
+newProduct.id = parseInt(newProduct.id);
+newProduct.Quantity = parseInt(newProduct.Quantity)
 
-    // Obtener el último código de carrito y sumarle 1 para generar un nuevo código único
-    const lastCart = carts[carts.length - 1];
-    const cartcode = lastCart ? lastCart.cartcode + 1 : 1;
+cartproducts.push(newProduct);
 
-    // Crear un nuevo objeto de carrito con el producto agregado y el código generado
-    const cart = {
-        cartcode,
-        cartproducts: [newProduct]
-    };
+// Leer el archivo de carritos
+const carts = JSON.parse(fs.readFileSync(cartsFilePath));
 
-    // Agregar el carrito al array de carritos
-    carts.push(cart);
+// Obtener el último código de carrito y sumarle 1 para generar un nuevo código único
+const lastCart = carts[carts.length - 1];
+const cartcode = lastCart ? lastCart.cartcode + 1 : 1;
 
-    // Guardar el archivo de carritos
-    fs.writeFileSync(cartsFilePath, JSON.stringify(carts));
+// Crear un nuevo objeto de carrito con el producto agregado y el código generado
+const cart = {
+  cartcode,
+  cartproducts: [newProduct]
+};
 
-    // Devolver la respuesta con el carrito creado y el código de carrito generado
-    res.json({
-        cartcode,
-        cartproducts: [newProduct]
-    });
+// Agregar el carrito al array de carritos
+carts.push(cart);
+
+// Guardar el archivo de carritos
+fs.writeFileSync(cartsFilePath, JSON.stringify(carts));
+
+// Devolver la respuesta con el carrito creado y el código de carrito generado
+res.json({
+  cartcode,
+  cartproducts: [newProduct]
+});
+
 });
 
 
 
 router.post('/:cartcode/product/:pid', (req, res) => {
-    const { cartcode, pid } = req.params;
-  
-    // Leer carts.json
-    const cartsData = fs.readFileSync(cartsFilePath);
-    const carts = JSON.parse(cartsData);
-  
-    // Buscar el objeto con el cartcode indicado en la URL
-    const cartIndex = carts.findIndex((cart) => cart.cartcode === parseInt(cartcode));
-  
-    if (cartIndex === -1) {
-      res.status(404).json({ message: `El carrito con código ${cartcode} no existe.` });
-      return;
-    }
-  
-    // Leer products.json
-    const productsData = fs.readFileSync(productsFilePath);
-    const products = JSON.parse(productsData);
+  const { cartcode, pid } = req.params;
 
-    // Buscar el objeto con el pid indicado en la URL
-    const productId = parseInt(pid);
-    const productIndex = products.findIndex((product) => product.id === productId);
-    
+  const cartsData = fs.readFileSync(cartsFilePath);
+  const carts = JSON.parse(cartsData);
 
-    if (productIndex === -1) {
-      res.status(404).json({ message: `El producto con ID ${pid} no existe.` });
-      return;
-    }
-  
-    // Buscar si el producto ya está en el carrito
-    const existingProductIndex = carts[cartIndex].cartproducts.findIndex((product) => product.id === pid);
-  
-    if (existingProductIndex !== -1) {
-      // Si el producto ya está en el carrito, aumenta la cantidad en 1
-      carts[cartIndex].cartproducts[existingProductIndex].Quantity += 1;
-    } else {
-      // Si el producto no está en el carrito, agrega el producto al carrito con una cantidad de 1
-      carts[cartIndex].cartproducts.push({
-        id: products[productIndex].id,
-        titulo: products[productIndex].titulo,
-        descripcion: products[productIndex].descripcion,
-        codigo: products[productIndex].codigo,
-        precio: products[productIndex].precio,
-        stock: products[productIndex].stock,
-        estado: products[productIndex].estado,
-        categoria: products[productIndex].categoria,
-        Quantity: 1
+  const cartIndex = carts.findIndex((cart) => cart.cartcode === parseInt(cartcode));
+
+  if (cartIndex === -1) {
+    res.status(404).json({ message: `El carrito con código ${cartcode} no existe.` });
+    return;
+  }
+
+  const productsData = fs.readFileSync(productsFilePath);
+  const products = JSON.parse(productsData);
+
+  const productIndex = products.findIndex((product) => product.id === parseInt(pid));
+
+  if (productIndex === -1) {
+    res.status(404).json({ message: `El producto con ID ${pid} no existe.` });
+    return;
+  }
+
+  const existingProductIndex = carts[cartIndex].cartproducts.findIndex((product) => product.id === parseInt(pid));
+
+  if (existingProductIndex !== -1) {
+    carts[cartIndex].cartproducts[existingProductIndex].Quantity += 1;
+  } else {
+    carts[cartIndex].cartproducts.push({
+      ...products[productIndex],
+      Quantity: 1
     });
-    
-    }
-  
-    fs.writeFileSync(cartsFilePath, JSON.stringify(carts));
-  
-    res.status(200).json({ message: `El producto con ID ${pid} fue agregado al carrito con código ${cartcode}.` });
+  }
+
+  fs.writeFileSync(cartsFilePath, JSON.stringify(carts));
+
+  res.status(200).json({ message: `El producto con ID ${pid} fue agregado al carrito con código ${cartcode}.` });
 });
-  
-  
+
 
 export default router;
